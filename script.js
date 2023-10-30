1362ffe9e302d56bf390f0c84b41cdee8823f3f8
chitra0404 <chitrasuresh0404@gmail.com>
2023-10-30 23:33:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const count=document.querySelector(".count");
+setTimeout(function(){
+    count.innerText="10";
+    setTimeout(function(){
+        count.innerText="9";
+        setTimeout(function(){
+            count.innerText="8";
+            setTimeout(function(){
+                count.innerText="7";
+                setTimeout(function(){
+                    count.innerText="6";
+                    setTimeout(function(){
+                        count.innerText="5";
+                        setTimeout(function(){
+                            count.innerText="4";
+                            setTimeout(function(){
+                                count.innerText="3";
+                                setTimeout(function(){
+                                    count.innerText="2";
+                            
+                               setTimeout(function(){
+                                        count.innerText="Happy Independence Day";
+                                    
+                                    }
+                                        ,1000);
+                                    },1000);
+                                    },1000);
+                                
+                                },1000);
+                            } ,1000);
+                },1000);
+        
+        },1000);
+    },1000);
+},1000);
+},1000);
